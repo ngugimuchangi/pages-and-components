1b918df1f9f84d018b7bb23b85285010ca5641ed
ngugimuchangi <tech.curious14@gmail.com>
2023-06-04 19:49:29 +0300

    completed project

---
+++ easybank-landing-page/README.md
@@ -0,0 +1,50 @@
+# Interactive card details form solution
+
+This is a solution to the [Easybank landing page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/easybank-landing-page-WaUhkoDN).
+
+## Table of contents
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshots](#screenshots)
+  - [Links](#links)
+- [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+
+### Screenshots
+
+<div align="center">
+
+#### Small Devices
+![](images/small-screens.png)
+![](images/small-screens-menu-active.png)
+
+#### Medium Size Devices
+![](images/medium-screens.png)
+
+
+#### Large
+![](images/large-screens.png)
+</div>
+
+### Links
+- [Solution URL](https://github.com/ngugimuchangi/frontend_mentors/tree/master/easybank-landing-page)
+- [Demo]()
+
+## Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Sass
+
+## Author
+- [Duncan Ngugi](https://github.com/ngugimuchangi)
---
+++ easybank-landing-page/assets/js/index.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', () => {
+// ELEMENTS
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const menu = document.querySelector('.main-nav');
+  const columns = document.querySelectorAll('.col');
+  const media = window.matchMedia('(min-width: 960px)');
+
+  // FUNCTIONS
+  /**
+   * Toggle hamburger menu
+   */
+  function toggleMenu() {
+    hamburgerMenu.classList.toggle('opened');
+    menu.classList.toggle('visible');
+    menu.classList.toggle('slide-out', !menu.classList.contains('visible'));
+    if (hamburgerMenu.attributes['aria-expanded'].value === 'true') hamburgerMenu.attributes['aria-expanded'].value = 'false';
+    else hamburgerMenu.attributes['aria-expanded'].value = 'true';
+  }
+
+  /**
+   * Observe sections animate when it enters viewport
+   * @param {object} entries
+   */
+  function animateOnScroll(entries) {
+    entries.forEach((entry) => {
+      if (!media.matches) this.unobserve(entry.target);
+      if (entry.isIntersecting) {
+        entry.target.classList.toggle('slide', media.matches);
+      }
+    });
+  }
+
+  // OBSERVERS
+  const columnObserver = new IntersectionObserver(animateOnScroll);
+  columns.forEach((col) => columnObserver.observe(col));
+
+  // EVENTS
+  hamburgerMenu.addEventListener('click', toggleMenu);
+  window.addEventListener('resize', () => {
+    if (media.matches) menu.classList.remove('slide-out');
+  });
+});
