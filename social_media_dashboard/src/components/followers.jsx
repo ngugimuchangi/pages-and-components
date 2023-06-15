19036026abf61b39ff408083b49b91536ae87df9
ngugimuchangi <tech.curious14@gmail.com>
2023-06-15 09:59:55 +0300

    removed sections

--- social_media_dashboard/src/components/followers.jsx
+++ social_media_dashboard/src/components/followers.jsx
@@ -4,10 +4,10 @@ export default function FollowersSection({ platforms }) {
   const followersCards = platforms.map((userDetails) => (
     <FollowersCard key={userDetails.platform} userDetails={userDetails}/>));
   return (
-    <section className='followers-section'>
+    <div className='followers-section'>
       <div className='container flex-row'>
         {followersCards}
       </div>
-    </section>
+    </div>
   );
 }
