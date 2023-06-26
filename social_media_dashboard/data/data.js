03a6ebf9033cfba35089e495a1278d76fa0044bd
ngugimuchangi <tech.curious14@gmail.com>
2023-06-26 21:09:35 +0300

    moved data to individual file

---
+++ social_media_dashboard/data/data.js
@@ -0,0 +1,44 @@
+const data = {
+  facebook: {
+    platform: 'facebook',
+    username: '@nathanf',
+    followers: 1987,
+    followersToday: 12,
+    viewsOrRetweets: 87,
+    viewsOrRetweetsPerformance: 3,
+    Likes: 52,
+    LikesPerformance: -2,
+  },
+  twitter: {
+    platform: 'twitter',
+    username: '@nathanf',
+    followers: 1044,
+    followersToday: 99,
+    viewsOrRetweets: 117,
+    viewsOrRetweetsPerformance: 303,
+    Likes: 507,
+    LikesPerformance: 553,
+  },
+  instagram: {
+    platform: 'instagram',
+    username: '@realnathanf',
+    followers: 11000,
+    followersToday: 1099,
+    viewsOrRetweets: 52000,
+    viewsOrRetweetsPerformance: 1375,
+    Likes: 5462,
+    LikesPerformance: 2257,
+  },
+  youtube: {
+    platform: 'youtube',
+    username: 'Nathan F.',
+    followers: 8239,
+    followersToday: -144,
+    viewsOrRetweets: 1407,
+    viewsOrRetweetsPerformance: -12,
+    Likes: 107,
+    LikesPerformance: -19,
+  },
+};
+
+export default data;
