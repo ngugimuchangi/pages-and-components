3c5b14d0fa2083157c1490918d4990f938bba575
ngugimuchangi <tech.curious14@gmail.com>
2023-07-03 17:57:43 +0300

    moved project to own repo

--- rest-countries-api/src/components/layout/Container.jsx
+++
@@ -1,5 +0,0 @@
-function Container({ children }) {
-  return <div className='wrapper'> {children}</div>;
-}
-
-export default Container;
--- rest-countries-api/src/utils/fetchData.js
+++
@@ -1,7 +0,0 @@
-export default async function fetchData(url) {
-  return new Promise((resolve) => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => resolve(data));
-  });
-}
